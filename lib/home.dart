632fcf0fa2a4d781f325476b8596b1a3202470b2
mohdi <mohdinzamam12125@gmail.com>
2024-01-17 18:18:30 +0530

    Day 8 - Api fetching problem

--- lib/home.dart
+++ lib/home.dart
@@ -1,5 +1,8 @@
+import 'dart:convert';
 import 'package:flutter/material.dart';
 import 'package:carousel_slider/carousel_slider.dart';
+import 'package:news_short/model.dart';
+import 'package:http/http.dart';
 
 class Home extends StatefulWidget {
   const Home({super.key});
@@ -9,10 +12,28 @@ class Home extends StatefulWidget {
 }
 
 class _HomeState extends State<Home> {
-  final image1 =
-      "https://img.freepik.com/free-vector/global-technology-earth-news-bulletin-background_1017-33687.jpg?w=1380&t=st=1705422365~exp=1705422965~hmac=63b7a112fd8d410a3da7f51c566dc598e58d81d15044178ecc64050400d4f8c7";
+  final image1 = "https://img.freepik.com/free-vector/global-technology-earth-news-bulletin-background_1017-33687.jpg?w=1380&t=st=1705422365~exp=1705422965~hmac=63b7a112fd8d410a3da7f51c566dc598e58d81d15044178ecc64050400d4f8c7";
   TextEditingController searchController = TextEditingController();
+  List<NewsQueryModel> newsModelList = <NewsQueryModel>[];
   List<String> navBarItem = ["Top News", "India", "World", "Finance", "Health"];
+
+  bool isLoading = true;
+  getNewsByQuery(String query) async{
+    String url = "https://newsapi.org/v2/everything?q=$query&from=2023-12-17&sortBy=publishedAt&apiKey=9a316991d02c4794ad7d35e42ccf8d67";
+    Response response = await get(Uri.parse(url));
+    Map data = jsonDecode(response.body);
+    setState(() {
+      data["articles"].forEach((element){
+        NewsQueryModel newsQueryModel = new NewsQueryModel();
+        newsQueryModel = NewsQueryModel.fromMap(element);
+        newsModelList.add(newsQueryModel);
+        setState(() {
+          isLoading = false;
+        });
+      });
+    });
+
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -76,70 +97,121 @@ class _HomeState extends State<Home> {
                     );
                   }),
             ),
+            const SizedBox(height: 15),
             CarouselSlider(
-                items: items.map((item) {
+                items: newsModelList.map((instance) {
                   return Builder(builder: (BuildContext context) {
-                    return InkWell(
-                      onTap: () {},
-                      child: Container(
-                        margin: const EdgeInsets.symmetric(
-                            horizontal: 5, vertical: 15),
-                        decoration: BoxDecoration(color: item),
+                    return Container(
+                      child: Card(
+                        child: Stack(
+                          children: [
+                            ClipRRect(
+                              borderRadius: BorderRadius.circular(15),
+                              child: Image.network(instance.newsImg,
+                                  fit: BoxFit.fitHeight,
+                                  height: double.infinity),
+                            ),
+                            Positioned(
+                                left: 0,
+                                right: 0,
+                                bottom: 0,
+                                child: Container(
+                                  decoration: BoxDecoration(
+                                      gradient: LinearGradient(
+                                          colors: [
+                                        Colors.white.withOpacity(0),
+                                        Colors.white
+                                      ],
+                                          begin: Alignment.topCenter,
+                                          end: Alignment.bottomCenter)),
+                                  padding: const EdgeInsets.all(10),
+                                  child: const Text(
+                                    "News are here!!",
+                                    style: TextStyle(
+                                        color: Colors.white,
+                                        fontSize: 15,
+                                        fontWeight: FontWeight.bold),
+                                  ),
+                                )),
+                          ],
+                        ),
                       ),
                     );
                   });
                 }).toList(),
                 options: CarouselOptions(
                     height: 200, autoPlay: true, enableInfiniteScroll: false)),
-            Container(
-              child: ListView.builder(
-                  itemCount: 3,
-                  shrinkWrap: true,
-                  itemBuilder: (context, index) {
-                    return Container(
-                      margin: const EdgeInsets.symmetric(
-                          vertical: 5, horizontal: 10),
-                      child: ClipRRect(
-                        borderRadius: BorderRadius.circular(30),
-                        child: Card(
-                          elevation: 2.0,
-                          child: Stack(
-                            children: [
-                              Image.network(image1),
-                              Positioned(
-                                  left: 0,
-                                  right: 0,
-                                  bottom: 0,
-                                  child: Container(
-                                    padding: const EdgeInsets.all(15),
-                                    decoration: BoxDecoration(
-                                        gradient: LinearGradient(
-                                            colors: [
-                                          Colors.white.withOpacity(0),
-                                          Colors.white
+            const SizedBox(height: 10,),
+            Column(
+              children: [
+                const Padding(
+                  padding: EdgeInsets.all(15),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.start,
+                      children: [
+                        Text(
+                          "Latest News",
+                        style: TextStyle(
+                          fontWeight: FontWeight.bold,
+                          fontSize: 20
+                        ),)
+                      ]
+                  ),
+                ),
+                ListView.builder(
+                    physics: const NeverScrollableScrollPhysics(),
+                    itemCount: newsModelList.length,
+                    shrinkWrap: true,
+                    itemBuilder: (context, index) {
+                      return Container(
+                        margin: const EdgeInsets.symmetric(
+                            vertical: 5, horizontal: 10),
+                        child: ClipRRect(
+                          borderRadius: BorderRadius.circular(30),
+                          child: Card(
+                            elevation: 2.0,
+                            child: Stack(
+                              children: [
+                                Image.network(newsModelList[index].newsImg),
+                                Positioned(
+                                    left: 0,
+                                    right: 0,
+                                    bottom: 0,
+                                    child: Container(
+                                      padding: const EdgeInsets.all(15),
+                                      decoration: BoxDecoration(
+                                          gradient: LinearGradient(
+                                              colors: [
+                                            Colors.white.withOpacity(0),
+                                            Colors.white
+                                          ],
+                                              begin: Alignment.topCenter,
+                                              end: Alignment.bottomCenter)),
+                                      child: Column(
+                                        crossAxisAlignment:
+                                            CrossAxisAlignment.start,
+                                        children: [
+                                          Text(
+                                            newsModelList[index].newsHead,
+                                            style: const TextStyle(
+                                                color: Colors.white,
+                                                fontSize: 20,
+                                                fontWeight: FontWeight.bold),
+                                          ),
+                                          Text(newsModelList[index].newsDes.length > 50 ? "${newsModelList[index].newsDes.substring(0,55)}.." : newsModelList[index].newsDes,)
                                         ],
-                                            begin: Alignment.topCenter,
-                                            end: Alignment.bottomCenter)),
-                                    child: const Column(
-                                      crossAxisAlignment: CrossAxisAlignment.start,
-                                      children: [
-                                        Text(
-                                          "News Headline",
-                                          style: TextStyle(
-                                              color: Colors.white,
-                                              fontSize: 20,
-                                              fontWeight: FontWeight.bold),
-                                        ),
-                                        Text("Blah Blah Blah .....")
-                                      ],
-                                    ),
-                                  ))
-                            ],
+                                      ),
+                                    ))
+                              ],
+                            ),
                           ),
                         ),
-                      ),
-                    );
-                  }),
+                      );
+                    }),
+                ElevatedButton(
+                    onPressed: (){},
+                    child: const Text("Show More"))
+              ],
             )
           ],
         ),
