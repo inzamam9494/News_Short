8e84ea59dacfd96cc5c9f12695e04f9f655df962
mohdi <mohdinzamam12125@gmail.com>
2024-02-26 17:55:48 +0530

    Day 31 - Facebook Authentication

--- lib/Authentication/services/auth_service.dart
+++ lib/Authentication/services/auth_service.dart
@@ -1,4 +1,5 @@
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter_facebook_auth/flutter_facebook_auth.dart';
 import 'package:google_sign_in/google_sign_in.dart';
 
@@ -34,12 +35,22 @@ class AuthService{
       }
     } on FirebaseAuthException catch (e) {
       // Handle Firebase authentication exceptions
-      print('Firebase Auth Exception: ${e.message}');
-      throw e; // rethrow the exception
+      if (kDebugMode) {
+        print('Firebase Auth Exception: ${e.message}');
+      }
+      rethrow; // rethrow the exception
     } catch (e) {
       // Handle other exceptions
-      print('Other Exception: $e');
-      throw e; // rethrow the exception
+      if (kDebugMode) {
+        print('Other Exception: $e');
+      }
+      rethrow; // rethrow the exception
     }
   }
 }
+
+// <meta-data android:name="com.facebook.sdk.ApplicationId" android:value="@string/facebook_app_id"/>
+// <meta-data android:name="com.facebook.sdk.ClientToken" android:value="@string/facebook_client_token"/>
+// <meta-data
+// android:name="flutterEmbedding"
+// android:value="2" />
