76a8d6cd1b40e4e8b638791fa72ffce5ff89f7c0
mohdi <mohdinzamam12125@gmail.com>
2024-03-05 22:08:50 +0530

    Day 34 - Profile UI changes

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:get/get_navigation/src/root/get_material_app.dart';
 import 'package:news_short/Authentication/auth_page.dart';
 import 'package:news_short/SampleCheking.dart';
 import 'package:news_short/View/home.dart';
@@ -24,7 +25,7 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
+    return GetMaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
